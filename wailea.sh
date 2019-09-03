36effcd8aeb6c2ed75e4d133f2fe452674f62974
Firizki <firizki.emsa@bukalapak.com>
2019-09-03 23:17:39 +0700

    Add handle for GET parameters

--- wailea.sh
+++ wailea.sh
@@ -7,7 +7,7 @@ while true
 do
   cat res_pipe | nc -lp 1080 -q 0 > >(
   if read line; then
-    REQUEST="responses/"$(echo "$line" | cut -d ' ' -f -2 | sed 's/ //g')"/response"
+    REQUEST="responses/"$(echo "$line" | cut -d ' ' -f -2 | sed 's/ //g' | cut -d '?' -f 1)"/response"
     if [[ -f "${REQUEST}" ]]; then
       cat ${REQUEST} > res_pipe
       break
